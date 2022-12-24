98ce68bbc56b27342190ca332656296d5e4b1d5c
Danil Tkachenko <tkachenkodanil06@gmail.com>
2022-12-24 17:25:49 +0200

    Fix bugs

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -3,6 +3,6 @@ const menu = document.querySelector('.menu');
 
 btn.addEventListener('click', function() {
 	btn.classList.toggle('active');
-	menu.classList.toggle('max-sm:translate-x-0');
+	menu.classList.toggle('active');
 	document.body.classList.toggle('overflow-hidden');
 });
